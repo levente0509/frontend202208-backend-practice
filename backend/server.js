865373756aa20bf555595c6e240972156e1fac04
ricsimarta <ricsimarta@gmail.com>
2022-11-07 10:21:19 +0100

    commit2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
@@ -8,5 +9,16 @@ app.get('/', (req, res) => {
 })
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
+
+app.get('/beers', (req, res) => {
+  fs.readFile(`${__dirname}/data/data.json`, (err, data) => {
+    if (err) {
+      console.log('hiba:', err)
+      res.status(500).send('hibavan')
+    } else {
+      res.status(200).send(JSON.parse(data))
+    }
+  })
+})
 
 app.listen(2022, console.log('server listening on http://127.0.0.1:2022'))
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,14 @@
 console.log('loaded')
+const rootElement = document.querySelector('#root')
+
+const beerComponent = (name, price, rating) => `
+  <div class="beer">
+    <h2>${name}</h2>
+    <h3>${price}</h3>
+    <h4>${rating}</h4>
+  </div>
+`
+
+fetch('/beers')
+  .then(res => res.json())
+  .then(beers => beers.map((beer) => rootElement.insertAdjacentHTML('beforeend', beerComponent(beer.name, beer.price, beer.rating))))
